690aae99c30e86686caf21643ef627c5111123da
Reza Abasi <118380898+rezaabasi-dev@users.noreply.github.com>
2026-08-25 00:15:49 +0330

    Delete app directory

--- app/api/router.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.users import router as users_router
-
-router = APIRouter()
-router.include_router(users_router, prefix="/api/v1")
--- app/api/v1/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.core.security import create_token
-
-router = APIRouter()
-
-@router.post("/login")
-def login(username: str):
-    token = create_token({"sub": username})
-    return {"access_token": token}
